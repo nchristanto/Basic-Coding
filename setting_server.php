6e0aef1fc6d69f89448431ea259ee792200c4249
nchrista835 <christanto.nanda@gmail.com>
2018-09-27 11:41:32 +0700

    first commit

---
+++ setting_server.php
@@ -0,0 +1,9 @@
+<?php
+    define('server','localhost');
+    define('user','root');
+    define('pass','');
+    define('db', 'kulina');
+
+    $con = mysqli_connect(server, user, pass, db);
+    
+?>
